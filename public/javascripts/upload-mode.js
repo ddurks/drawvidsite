4721c86ff66e4d97f137d598e01f0a88ecdcec0f
ddurks <dldurks@gmail.com>
2019-02-06 15:47:59 -0600

    update

--- public/javascripts/upload-mode.js
+++ public/javascripts/upload-mode.js
@@ -6,18 +6,7 @@ function arrayBufferToString(array) {
     var decoder = new TextDecoder("utf-8");
     return decoder.decode(array);
 }
-function arraysEqual(arr1, arr2) {
-    if(arr1.length !== arr2.length)
-        return false;
-    for(var i = arr1.length; i--;) {
-        if(arr1[i] !== arr2[i])
-            return false;
-    }
-  
-    return true;
-  }
 
-var passwordHash = new Uint8Array([246, 176, 40, 61, 226, 146, 153, 234, 219, 213, 198, 246, 137, 87, 78, 149, 181, 90, 33, 83, 62, 67, 245, 142, 44, 166, 98, 53, 202, 1, 117, 199, 147, 23, 53, 132, 71, 43, 210, 17, 169, 207, 225, 233, 216, 198, 111, 61, 120, 19, 183, 217, 164, 93, 200, 147, 203, 121, 94, 108, 232, 201, 232, 121]);
 var password;
 
 function upload() {
