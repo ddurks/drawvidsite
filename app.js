59c0a85263b805161bdc031997cf523b6ac8d284
David Durkin <dldurks@gmail.com>
2019-02-13 22:04:10 -0600

    please work

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
